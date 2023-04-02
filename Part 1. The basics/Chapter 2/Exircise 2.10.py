1085ebd17ebbe35d41905115c755a085cd970f8f
Eugene Kyoto <eugenebubnov@vk.com>
2023-04-02 12:12:26 +0300

    Minor changes

--- Part 1. The basics/Chapter 2/Exircise 2.10.py
+++ Part 1. The basics/Chapter 2/Exircise 2.10.py
@@ -3,4 +3,4 @@ name = 'eugene eubnov' # Имя пользователя, тип данных st
 age = 26 # Возраст пользователя, тип данных: int.
 bank_account = 220.10 # Сумма денег на банк аккаунте, тип данных: float.
 
-print(f'Welcome to your bank account {name.title()}!\nToday you turn 26 years old!\nYour balance: {bank_account}')
+print(f'Welcome to your bank account {name.title()}!\nToday you turn 26 years old! Happy birthday!\nYour balance: {bank_account}')
