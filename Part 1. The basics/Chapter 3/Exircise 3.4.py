f0a728a3b7290fcea2c66f70e9b4d05efe163f85
Eugene Kyoto <eugenebubnov@vk.com>
2023-04-16 14:17:48 +0300

    Exircise 3.4

---
+++ Part 1. The basics/Chapter 3/Exircise 3.4.py
@@ -0,0 +1,6 @@
+# 3.4 Список гостей.
+
+guest_list = ['Pavel', 'Anton', 'Vlad', 'Zexkillo', 'Alex']
+
+for guest in guest_list:
+    print(f'Hey man, it"s invite for you {guest}.\nYour friend, Eugene!\n')
