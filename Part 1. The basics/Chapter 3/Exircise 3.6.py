5cc0daf54ec8b7a31206ba8e3b0676731163d4e1
Eugene Kyoto <eugenebubnov@vk.com>
2023-04-19 16:41:52 +0300

    add Exircise 3.6

---
+++ Part 1. The basics/Chapter 3/Exircise 3.6.py
@@ -0,0 +1,12 @@
+# 3.6 Больше гостей
+guest_list = ['Pavel', 'Anton', 'Vlad', 'Zexkillo', 'Alex']
+
+guest_list.insert(0, 'Vladimir') # Добавить нового гостя в начало списка.
+guest_list.insert(3, 'Lex') # Добавить в середину списка.
+guest_list.append('OG') # Добавить в конец списка.
+
+for guest in guest_list:
+    if guest == 'Vladimir' or guest == 'Lex' or guest == 'OG':
+        print(f'Welcome {guest}! Nice to meet u!')
+    else:
+        print(f'Ты уже приглашён, {guest}!')
