5cf21980852e03dc14343cd768676235d5ce60bc
Eugene Kyoto <eugenebubnov@vk.com>
2023-04-22 16:07:25 +0300

    Exircise 3.7

---
+++ Part 1. The basics/Chapter 3/Exirsice 3.7.py
@@ -0,0 +1,15 @@
+# 3.7 Сокращение списка гостей.
+guest_list = ['Pavel', 'Anton', 'Vlad', 'Zexkillo', 'Alex']
+
+for i in guest_list:
+    guest_list.pop()
+
+print(guest_list)
+
+del guest_list[0]
+
+print(guest_list)
+
+del guest_list[0]
+
+print(guest_list)
