7c270c276333e8f9c3c726cfc4b49830ce14dc3f
Eugene Kyoto <eugenebubnov@vk.com>
2023-04-23 07:23:37 +0300

    Exercise 3.8

---
+++ Part 1. The basics/Chapter 3/Exircise 3.8.py
@@ -0,0 +1,19 @@
+# 3.8 Повидать мир
+countries = ['Japan', 'South Korea', 'Great Britain', 'UAE', 'USA', 'Canada', 'Finland', 'Serbia', 'Africa']
+print(countries, sorted(countries), countries, sep='\n')
+print()
+
+countries.reverse()
+print(countries)
+print()
+
+countries.reverse()
+print(countries)
+print()
+
+countries.sort()
+print(countries)
+print()
+
+countries.sort(reverse=True)
+print(countries)
---
+++ Part 1. The basics/Chapter 3/Exircise 3.9.py
@@ -0,0 +1,3 @@
+# 3.9 Количество гостей (сделаю кол-во стран).
+countries = ['Japan', 'South Korea', 'Great Britain', 'UAE', 'USA', 'Canada', 'Finland', 'Serbia', 'Africa']
+print(len(countries))
