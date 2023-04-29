8d3c39ccf6d126f7134f6676697c77c9b448a1cd
Eugene Kyoto <eugenebubnov@vk.com>
2023-04-29 16:30:55 +0300

    Exercise 4.1 and 4.2

---
+++ Part 1. The basics/Chapter 4/ExErcise 4.2.py
@@ -0,0 +1,6 @@
+# 4.2 Животные.
+animals = ['Cat Stan', 'Dog Barry', 'Parrot Parley']
+
+for animal in animals:
+    print(f'A {animal} would make a great pet!')
+print('\nAny of these animals would make a great pet!')
---
+++ Part 1. The basics/Chapter 4/Exercise 4.1.py
@@ -0,0 +1,6 @@
+# 4.1 Пицца.
+pizzas = ['Пепперони', 'Маргарита', '4 сыра']
+
+for pizza in pizzas:
+    print(f'I like {pizza} pizza!')
+    print('I really love pizza :)\n')
