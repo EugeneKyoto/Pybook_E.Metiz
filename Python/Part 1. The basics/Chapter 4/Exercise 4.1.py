50bc216fc4740b6845c8f590898759ae4c05f0b2
Eugene Kyoto <eugenebubnov@vk.com>
2023-05-01 18:03:42 +0300

    Minor changes +activity

--- Python/Part 1. The basics/Chapter 4/Exercise 4.1.py
+++ Python/Part 1. The basics/Chapter 4/Exercise 4.1.py
@@ -3,4 +3,4 @@ pizzas = ['Пепперони', 'Маргарита', '4 сыра']
 
 for pizza in pizzas:
     print(f'I like {pizza} pizza!')
-    print('I really love pizza :)\n')
+    print('I really love pizza! :)\n')
