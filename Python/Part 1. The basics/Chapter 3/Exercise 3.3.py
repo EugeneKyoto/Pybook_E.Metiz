cf961ecc630ca9b663bf5734e64881d93065e401
Eugene Kyoto <eugenebubnov@vk.com>
2023-05-14 06:39:53 +0300

    Minor changes

--- Python/Part 1. The basics/Chapter 3/Exercise 3.3.py
+++ Python/Part 1. The basics/Chapter 3/Exercise 3.3.py
@@ -1,4 +1,7 @@
 cars = ['Mitsubishi', 'BMW', 'Audi', 'Vortex', 'VAZ']
 
 for car in cars:
-    print(f'I wonna buy {car}')
+    if car == 'Mitsubishi' or car == 'BMW' or car == 'Audi' or car == 'Vortex':
+        print(f'I wonna buy {car}')
+    else:
+        print(f'I don"t wonna buy {car}...')
--- Python/Part 1. The basics/Chapter 4/Exercise 4.5.py
+++ Python/Part 1. The basics/Chapter 4/Exercise 4.5.py
@@ -1,6 +1,6 @@
 # 4.5 Суммирование чисел (min, max, sum)
 
-lst = [i for i in range(50)]
+lst = [i for i in range(51)]
 print(max(lst))
 print(min(lst))
 print(sum(lst))
