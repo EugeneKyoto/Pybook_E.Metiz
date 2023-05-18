381b0fc75ef285c113bc6b17205804c8fc9e797c
Eugene Kyoto <eugenebubnov@vk.com>
2023-05-18 21:00:09 +0300

    Minor changes

--- Python/Part 1. The basics/Chapter 5/Exercise 5.10.py
+++ Python/Part 1. The basics/Chapter 5/Exercise 5.10.py
@@ -1,1 +1,3 @@
-print('hello world')
+# 5.10 не могу пока понять, отработал 14 часов...Отложу на завтра.
+current_users = ['Eric', 'Eugene', 'Pavel', 'Anton', 'Alex', 'Vlad', 'Vitaliy', 'Yuriy']
+new_users = []
