f6949e0084c81e08cf88e4cd60bdac8191c00d50
Eugene Kyoto <eugenebubnov@vk.com>
2023-05-19 08:21:24 +0300

    Exercise 5.10

--- Python/Part 1. The basics/Chapter 5/Exercise 5.10.py
+++ Python/Part 1. The basics/Chapter 5/Exercise 5.10.py
@@ -1,3 +1,16 @@
-# 5.10 не могу пока понять, отработал 14 часов...Отложу на завтра.
+# 5.10 Проверка имён пользователей.
 current_users = ['Eric', 'Eugene', 'Pavel', 'Anton', 'Alex', 'Vlad', 'Vitaliy', 'Yuriy']
-new_users = []
+new_users = ['Eugene', 'Pavel', 'John', 'Michael', 'Lui']
+
+for user in current_users:
+    if user in new_users:
+        print(f'{user} - это имя уже используется!')
+    else:
+        print(f'{user.lower()} - это имя свободно!')
+
+current_users2 = current_users.copy()
+current_users2.append('John')
+for i in current_users2:
+    if i.lower() == 'john':
+        print(f'\n{i.upper()} - already used.\n')
+print(current_users2)
