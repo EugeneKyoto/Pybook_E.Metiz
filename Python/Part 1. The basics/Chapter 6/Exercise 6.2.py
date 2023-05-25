d33db3db78bea4c9e6cbd9b874cb372771318628
Eugene Kyoto <eugenebubnov@vk.com>
2023-05-25 16:27:42 +0300

    Exercise 6.2

---
+++ Python/Part 1. The basics/Chapter 6/Exercise 6.2.py
@@ -0,0 +1,9 @@
+# 6.2 Любимые ЯП.
+languages = {
+    'eugene' : 'python',
+    'anton' : 'c++',
+    'slave"ik' : 'javascript'
+}
+
+for name, language in languages.items():
+    print(f'{name.title()} likes to program in {language.title()}!')
